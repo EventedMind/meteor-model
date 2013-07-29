6a7ade43e9369b3533be7455e73d5cbf4bc9b3a4
Chris Mather <mather.chris@gmail.com>
2013-07-29 09:30:32 -0700

    Add EJSON dependency.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe('Simple models');
 
 Package.on_use(function (api) {
   var where = ['client', 'server'];
-  api.use(['reactive-dict', 'mongo-livedata', 'underscore'], where);
+  api.use(['ejson', 'reactive-dict', 'mongo-livedata', 'underscore'], where);
   api.add_files(['model.js'], where);
   api.export('Model', where);
 });
